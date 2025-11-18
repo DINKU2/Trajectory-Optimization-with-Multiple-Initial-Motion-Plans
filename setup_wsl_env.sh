c9bb133b3964a9c01dc6567a4fc4aaae5a59a975
DINKU2 <99506366+DINKU2@users.noreply.github.com>
2025-11-17 22:23:44 -0600

    hide

--- setup_wsl_env.sh
+++ setup_wsl_env.sh
@@ -1,6 +1,4 @@
-# cd /mnt/c/Users/Toxic/Desktop/Code/Trajectory-Optimization-with-Multiple-Initial-Motion-Plans
-# put your project directory here
-
+# cd your directory here
 python3 -m venv traj_opt_env_wsl
 source traj_opt_env_wsl/bin/activate
 
